f1154a8ad4ff6daeb6bfe95bcdfb6a7e3b93653b
Sukanta Purkayastha <spurkayastha83@gmail.com>
2022-07-13 01:50:15 +0600

    update sidebar color admin

--- admin/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Online Library <?php echo date('Y') ?></span>
-        </div>
-    </div>
-</footer>
