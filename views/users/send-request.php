6498dc75d14189c6fb3dc5cb64194912cbc2ee1b
Sukanta Purkayastha <spurkayastha83@gmail.com>
2022-08-10 02:59:07 +0600

    request page ready

--- views/users/send-request.php
+++ views/users/send-request.php
@@ -3,64 +3,117 @@ use Illuminate\Support\Str;
 ?>
 <?= include_page('shared/user/header') ?>
 
-
-  <!-- category section -->
-  <section class="catagory_section layout_padding">
+<!-- category section -->
+<section class="catagory_section layout_padding">
     <div class="catagory_container">
-      <div class="container ">
-        <div class="heading_container heading_center">
-          <h2>
-            Send a request for a book
-          </h2>
-        </div>
-        <div class="row">
-          <div class="col-md-9 col-sm-8">
-            <div class="row">
-                <?php 
-                    $image =  
-                          $book->image == 'default.jpg' ? 
-                          'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png' : 
-                          public_path('uploads/books/'.$book->image)
-                        ;
-                ?>
-                <div class="col-md-4">
-                  <div class="card h-100 shadow-sm">
-                  <img src="<?= $image ?>" class="card-img-top" alt="..." />
-                  <div class="card-body">
-                      <div class="clearfix mb-3">
-                        <span class="float-start badge rounded-pill bg-primary text-white">
-                          Available Book: 
-                          <?= $book->availability ?>
-                        </span> 
-                        <!-- <span class="float-right price-hp">12354.00&euro;</span> -->
-                      </div>
-                      <h5 class="card-title">
-                          <small><b class="text-bold">Title:</b> <?= $book->name ?></small><br>
-                          <small><b>Category:</b> <?= $book->category->name ?></small><br>
-                        <small><b>Author:</b> <?= $book->author ?></small>
-                      </h5>
-                  </div>
-                </div>
-              </div>
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    Send a request for a book
+                </h2>
             </div>
-          </div>
-          <div class="col-sm-4 col-md-3 sidebar">
-            <div class="list-group">
-                <span href="#" class="list-group-item active">
-                    You Can also try this
-                </span>
-                <?php foreach ($books as $key => $value) { ?>
-                  <a href="<?= URI('/book/'.$value->id.'/send-request/'.Str::slug($value->name)) ?>" class="list-group-item">
-                    <i class="fa fa-folder"></i> <?= $value->name ?> <span class="badge pull-right"><?= $value->availability ?></span>
-                  </a>
-                <?php } ?>
-            </div>        
-          </div>
-	      </div>
-      </div>
+            <div class="row">
+                <div class="col-md-9 col-sm-8">
+                    <div class="row">
+                        <?php 
+                    $image =  
+                          $book->image == 'default.jpg' ? 'https://www.freepnglogos.com/uploads/notebook-png/download-laptop-notebook-png-image-png-image-pngimg-2.png' : public_path('uploads/books/'.$book->image) ; ?>
+                        <div class="col-md-3">
+                            <div class="card h-100 shadow-sm">
+                                <img src="<?= $image ?>" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <div class="clearfix mb-3">
+                                        <span class="float-start badge rounded-pill bg-primary text-white">
+                                            Available Book:
+                                            <?= $book->availability ?>
+                                        </span>
+                                        <!-- <span class="float-right price-hp">12354.00&euro;</span> -->
+                                    </div>
+                                    <h5 class="card-title">
+                                        <small>
+                                            <b class="text-bold">Title:</b>
+                                            <?= $book->name ?>
+                                        </small>
+                                        <br />
+                                        <small>
+                                            <b>Category:</b>
+                                            <?= $book->category->name ?>
+                                        </small>
+                                        <br />
+                                        <small>
+                                            <b>Author:</b>
+                                            <?= $book->author ?>
+                                        </small>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <form action="<?= URI('/store-messages') ?>" method="post">
+                                <div class="form-group">
+                                    <label for="name"> Name</label>
+                                    <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    id="name" 
+                                    placeholder="Enter name"
+                                    value="<?= auth_user()['name'] ?>"
+                                    readonly 
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="name"> Contact No</label>
+                                    <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    id="contact" 
+                                    placeholder="Enter Contact No"
+                                    value="<?= auth_user()['contact'] ?? "" ?>"
+                                    required 
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="name"> Address</label>
+                                    <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    id="address" 
+                                    placeholder="Enter address"
+                                    value="<?= auth_user()['contact'] ?? "" ?>"
+                                    required 
+                                    />
+                                </div>
+                                <div class="">
+                                    <button type="submit" class="btn btn-sm btn-danger" id="btnContactUs">
+                                        Cancel
+                                    </button>
+                                    <button type="submit" class="btn btn-sm btn-primary pull-right" id="btnContactUs">
+                                        Send Request
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 sidebar">
+                    <div class="list-group">
+                        <span href="#" class="list-group-item active">
+                            You Can also try this
+                        </span>
+                        <?php foreach ($books as $key =>
+                        $value) { ?>
+                        <a href="<?= URI('/book/'.$value->id.'/send-request/'.Str::slug($value->name)) ?>" class="list-group-item">
+                            <i class="fa fa-folder"></i>
+                            <?= $value->name ?> <span class="badge pull-right"><?= $value->availability ?></span>
+                        </a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </section>
+</section>
 
-  <!-- end catagory section -->
+<!-- end catagory section -->
 
 <?= include_page('shared/user/footer') ?>
