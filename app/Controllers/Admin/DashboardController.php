7438682e8d070d8d3219ebd02b0afd7f749943a0
Sukanta Purkayastha <30552975+sukanta7660@users.noreply.github.com>
2022-08-13 19:13:34 +0600

    admin dashboard changed

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers\Admin;
 
+use App\Book;
+use App\BookIssue;
 use App\Controllers\Controller;
 
 class DashboardController extends Controller
@@ -9,6 +11,16 @@ class DashboardController extends Controller
 
   public function index()
   {
-    return view('admin/dashboard/index');
+      $requests = BookIssue::all();
+      $books = Book::all();
+      return view('admin/dashboard/index',[
+        'total' => count($requests),
+        'totalBooks' => count($books),
+        'pendingRequests' => $requests->where('status', 'pending')->count(),
+        'cancelledRequests' => $requests->where('status', 'cancelled')->count()
+      ]);
   }
 }
+
+// c3412534
+// rupali
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -15,6 +15,84 @@
           <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         </div>
 
+        <!-- Content Row -->
+        <div class="row">
+
+          <!-- Earnings (Monthly) Card Example -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                      Total Book</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalBooks ?></div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-book-open fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Earnings (Monthly) Card Example -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                      Total Request</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $total ?></div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-chevron-down fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Earnings (Monthly) Card Example -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                      Pending Request</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $pendingRequests ?></div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-lock-open fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Pending Requests Card Example -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                      Cancelled Requests</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $cancelledRequests ?></div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-user-ninja fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Content Row -->
+
       </div>
 
     </div>
