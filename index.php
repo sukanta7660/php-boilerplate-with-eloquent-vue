4fed502fe0c183958f4a127b505cac4c2b47c38a
Sukanta Purkayastha <spurkayastha83@gmail.com>
2023-07-10 09:51:45 +0600

    remove unused imported class

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-use Dotenv\Dotenv;
-use Phroute\Phroute\Dispatcher;
-use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
-use Phroute\Phroute\Exception\HttpRouteNotFoundException;
-use Phroute\Phroute\RouteCollector;
-use Phroute\Phroute\RouteParser;
-
 if (file_exists(__DIR__.'/vendor/autoload.php')) {
     require_once __DIR__.'/vendor/autoload.php';
 }
